d7da872a60dcb33ba70a75febb78dd89bb395f48
Sagrius <alexeibuic@gmail.com>
2025-08-09 18:17:16 +0300

    a

--- Assets/_Scripts/Scriptable/ResourceDeliveredGoal.cs
+++ Assets/_Scripts/Scriptable/ResourceDeliveredGoal.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-[CreateAssetMenu(fileName = "ResourceDeliveredGoal", menuName = "GOAP/Goals/Resource Delivered Goal")]
+[CreateAssetMenu(fileName = "ResourceDeliveredGoal", menuName = "Goap/Goals/Resource Delivered Goal")]
 public class ResourceDeliveredGoal : GoapGoal
 {
     public override HashSet<KeyValuePair<string, object>> GetGoalState()
